000bd7c7c8ba0341460759f864ab0fd55d3e3967
Rana Pratap Singh <108171374+Rana-Pratap@users.noreply.github.com>
2022-06-27 16:48:10 +0530

    Delete urls.py

--- loginPage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from loginPage import views
-
-urlpatterns = [
-    path('', views.home, name='home' ),
-    path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin'),
-    path('signout', views.signout, name='signout'),
-]
